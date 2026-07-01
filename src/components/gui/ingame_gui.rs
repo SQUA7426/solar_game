ee0899ce0cabcfcb25dbdfe6529ed94f265159d2
Timothy <timo-drexler@online.de>
2026-07-01 22:26:13 +0200

    Inside ingame_gui.rs -> extracted inner of button_system() into a new fn button_action()

--- src/components/gui/ingame_gui.rs
+++ src/components/gui/ingame_gui.rs
@@ -49,7 +49,7 @@ mod ingame {
     pub fn ingame_plugin(app: &mut App) {
         app.init_state::<DisplayState>()
             .add_systems(OnEnter(GameState::InGame), button_setup)
-            .add_systems(Update, (button_system).run_if(in_state(GameState::InGame)));
+            .add_systems(Update, (button_system, button_action).run_if(in_state(GameState::InGame)));
     }
 
     fn create_btn(h: f32, w: f32, m: f32, radius: f32) -> Node {
@@ -96,127 +96,125 @@ mod ingame {
     }
 
     pub fn button_setup(mut cmds: Commands) {
-            cmds.spawn((
-                DespawnOnExit(GameState::InGame),
-                create_node(80., 0., 25., Some(25.), 20., 50.),
-                children![(
-                    Node {
-                        width: percent(100.),
-                        flex_direction: FlexDirection::Column,
-                        align_self: AlignSelf::Center,
-                        justify_content: JustifyContent::Center,
-                        justify_items: JustifyItems::Center,
-                        ..default()
-                    },
-                    Children::spawn(SpawnIter(
-                        [
-                            (
-                                InGameButtonAction::Info,
-                                75.,
-                                90.,
-                                95.,
-                                6.,
-                                "INFO",
-                                NORMAL_BG
-                            ),
-                            (
-                                InGameButtonAction::CreateOrbit,
-                                75.,
-                                90.,
-                                95.,
-                                6.,
-                                "Orbit",
-                                NORMAL_BG
-                            ),
-                        ]
-                        .into_iter()
-                        .map(move |(bt, h, w, r, m, text, c)| {
-                            (
-                                Button,
-                                bt,
-                                create_btn(h, w, m, r),
-                                children![(create_btn_text(text.into()), InGameBTNText)],
-                                BackgroundColor(c),
-                                Outline {
-                                    width: px(6),
-                                    offset: px(0),
-                                    color: NORMAL_TEXT,
-                                },
-                            )
-                        }),
-                    )),
-                )],
-            ));
+        cmds.spawn((
+            DespawnOnExit(GameState::InGame),
+            create_node(80., 0., 25., Some(25.), 20., 50.),
+            children![(
+                Node {
+                    width: percent(100.),
+                    flex_direction: FlexDirection::Column,
+                    align_self: AlignSelf::Center,
+                    justify_content: JustifyContent::Center,
+                    justify_items: JustifyItems::Center,
+                    ..default()
+                },
+                Children::spawn(SpawnIter(
+                    [
+                        (
+                            InGameButtonAction::Info,
+                            75.,
+                            90.,
+                            95.,
+                            6.,
+                            "INFO",
+                            NORMAL_BG
+                        ),
+                        (
+                            InGameButtonAction::CreateOrbit,
+                            75.,
+                            90.,
+                            95.,
+                            6.,
+                            "Orbit",
+                            NORMAL_BG
+                        ),
+                    ]
+                    .into_iter()
+                    .map(move |(bt, h, w, r, m, text, c)| {
+                        (
+                            Button,
+                            bt,
+                            create_btn(h, w, m, r),
+                            children![(create_btn_text(text.into()), InGameBTNText)],
+                            BackgroundColor(c),
+                            Outline {
+                                width: px(6),
+                                offset: px(0),
+                                color: NORMAL_TEXT,
+                            },
+                        )
+                    }),
+                )),
+            )],
+        ));
 
-            cmds.spawn((
-                DespawnOnExit(GameState::InGame),
-                create_node(30., 30., 80., None, 40., 20.),
-                children![(
-                    Node {
-                        width: percent(100.),
-                        flex_direction: FlexDirection::Row,
-                        align_self: AlignSelf::Center,
-                        justify_content: JustifyContent::Center,
-                        justify_items: JustifyItems::Center,
-                        ..default()
-                    },
-                    Children::spawn(SpawnIter(
-                        [
-                            (
-                                InGameButtonAction::Fuse,
-                                75.,
-                                125.,
-                                95.,
-                                6.,
-                                "Fuse",
-                                NORMAL_BG
-                            ),
-                            (
-                                InGameButtonAction::Upgrade,
-                                75.,
-                                125.,
-                                95.,
-                                6.,
-                                "Upgrade",
-                                NORMAL_BG
-                            ),
-                            (
-                                InGameButtonAction::Combine,
-                                75.,
-                                125.,
-                                95.,
-                                6.,
-                                "Combine",
-                                NORMAL_BG
-                            ),
-                        ]
-                        .into_iter()
-                        .map(move |(bt, h, w, r, m, text, c)| {
-                            (
-                                Button,
-                                bt,
-                                create_btn(h, w, m, r),
-                                children![(create_btn_text(text.into()), InGameBTNText)],
-                                BackgroundColor(c),
-                                Outline {
-                                    width: px(6),
-                                    offset: px(0),
-                                    color: NORMAL_TEXT,
-                                },
-                            )
-                        }),
-                    )),
-                )],
-            ));
+        cmds.spawn((
+            DespawnOnExit(GameState::InGame),
+            create_node(30., 30., 80., None, 40., 20.),
+            children![(
+                Node {
+                    width: percent(100.),
+                    flex_direction: FlexDirection::Row,
+                    align_self: AlignSelf::Center,
+                    justify_content: JustifyContent::Center,
+                    justify_items: JustifyItems::Center,
+                    ..default()
+                },
+                Children::spawn(SpawnIter(
+                    [
+                        (
+                            InGameButtonAction::Fuse,
+                            75.,
+                            125.,
+                            95.,
+                            6.,
+                            "Fuse",
+                            NORMAL_BG
+                        ),
+                        (
+                            InGameButtonAction::Upgrade,
+                            75.,
+                            125.,
+                            95.,
+                            6.,
+                            "Upgrade",
+                            NORMAL_BG
+                        ),
+                        (
+                            InGameButtonAction::Combine,
+                            75.,
+                            125.,
+                            95.,
+                            6.,
+                            "Combine",
+                            NORMAL_BG
+                        ),
+                    ]
+                    .into_iter()
+                    .map(move |(bt, h, w, r, m, text, c)| {
+                        (
+                            Button,
+                            bt,
+                            create_btn(h, w, m, r),
+                            children![(create_btn_text(text.into()), InGameBTNText)],
+                            BackgroundColor(c),
+                            Outline {
+                                width: px(6),
+                                offset: px(0),
+                                color: NORMAL_TEXT,
+                            },
+                        )
+                    }),
+                )),
+            )],
+        ));
     }
 
-    use DisplayState::*;
 
     fn button_system(
-        mut button_query: Query<
+        mut btn_query: Query<
             (
                 &Interaction,
-                &InGameButtonAction,
                 &mut BackgroundColor,
                 &mut Outline,
                 &Children,
@@ -224,11 +222,38 @@ mod ingame {
             (Changed<Interaction>, With<Button>),
         >,
         mut text_query: Query<&mut TextColor>,
-        mut disp_state: ResMut<NextState<DisplayState>>,
     ) {
-        for (interaction, btn, mut bg_color, mut outline, children) in &mut button_query {
+        for (interaction, mut bg_color, mut outline, children) in &mut btn_query {
             let mut text_color = text_query.get_mut(children[0]).unwrap();
 
+            if *interaction == Interaction::Pressed {
+                *text_color = TextColor(PRESSED_TEXT);
+                *bg_color = BackgroundColor(PRESSED_BG);
+                outline.color = PRESSED_TEXT;
+            }
+            if *interaction == Interaction::Hovered {
+                *text_color = TextColor(HOVER_TEXT);
+                *bg_color = BackgroundColor(HOVER_BG);
+                outline.color = HOVER_TEXT;
+            }
+            if *interaction == Interaction::None {
+                *text_color = TextColor(NORMAL_TEXT);
+                *bg_color = BackgroundColor(NORMAL_BG);
+                outline.color = NORMAL_TEXT;
+            }
+        }
+    }
+
+    use DisplayState::*;
+
+    fn button_action(
+        mut btn_query: Query<
+            (&Interaction, &InGameButtonAction),
+            (Changed<Interaction>, With<Button>),
+        >,
+        mut disp_state: ResMut<NextState<DisplayState>>,
+    ) {
+        for (interaction, btn) in &mut btn_query {
             if *interaction == Interaction::Pressed {
                 match btn {
                     InGameButtonAction::Combine => {
@@ -247,10 +272,8 @@ mod ingame {
                         disp_state.set(DispUpgrade);
                     }
                 }
-                *text_color = TextColor(PRESSED_TEXT);
-                *bg_color = BackgroundColor(PRESSED_BG);
-                outline.color = PRESSED_TEXT;
             }
+
             if *interaction == Interaction::Hovered {
                 match btn {
                     InGameButtonAction::Combine => {
@@ -269,14 +292,9 @@ mod ingame {
                         disp_state.set(DispUpgrade);
                     }
                 }
-                *text_color = TextColor(HOVER_TEXT);
-                *bg_color = BackgroundColor(HOVER_BG);
-                outline.color = HOVER_TEXT;
             }
+
             if *interaction == Interaction::None {
-                *text_color = TextColor(NORMAL_TEXT);
-                *bg_color = BackgroundColor(NORMAL_BG);
-                outline.color = NORMAL_TEXT;
                 disp_state.reset();
             }
         }
